d446406e48e403a8d908e1a91801a91f4888ca4b
Your Name <you@example.com>
2023-04-26 15:44:20 +0300

    testfile

--- testfile.js
+++ testfile.js
@@ -1,10 +1,6 @@
 const mocha = require('mocha');
 const assert = require('assert');
 
-<<<<<<< HEAD
-=======
-// Подключение других файлов с тестами
->>>>>>> 899ba933b2c14798b5de472691ff9029082ad2bd
 require('./VerifyPriceFilter');
 require('./AddItemBasket');
 require('./SearchTheItem');

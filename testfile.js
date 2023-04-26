899ba933b2c14798b5de472691ff9029082ad2bd
Your Name <you@example.com>
2023-04-26 12:39:03 +0300

    mochawesome

---
+++ testfile.js
@@ -0,0 +1,14 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+// Подключение других файлов с тестами
+require('./VerifyPriceFilter');
+require('./AddItemBasket');
+require('./SearchTheItem');
+require('./CorrectProductBasket');
+
+describe('My test suite', function() {
+  it('should pass this test', function() {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
